d92d68cb7618ec4b20fa1d537f42642e32b665ae
Adam Howard <adam101howard@gmail.com>
2024-07-03 18:05:49 +0100

    add cycling digits to sliding grids showcase

--- p5-js/sliding-grids.js
+++ p5-js/sliding-grids.js
@@ -29,16 +29,6 @@ var RESOLUTION = 128;
 
 var points = [];
 
-for (let i=0; i<getRandomInt(1, 4); i++) {
-  createPoint(getRandomInt(1, PIXELS+1), getRandomInt(1, PIXELS+1), 50, 60);
-}
-for (let i=0; i<getRandomInt(1, 4); i++) {
-  createPoint(getRandomInt(1, PIXELS+1), getRandomInt(1, PIXELS+1), 120, 130);
-}
-for (let i=0; i<getRandomInt(1, 4); i++) {
-  createPoint(getRandomInt(1, PIXELS+1), getRandomInt(1, PIXELS+1), 190, 200);
-}
-
 function createPoint(x, y, start, end) {
   points.push(
     {
@@ -50,58 +40,207 @@ function createPoint(x, y, start, end) {
   );
 }
 
+function addChar(char, begin, end) {
+  switch (char) {
+    case '1':
+      createPoint(8, 6, begin+1, end+1);
+      createPoint(8, 7, begin+1, end+1);
+      createPoint(8, 8, begin, end);
+      createPoint(8, 9, begin, end);
+      createPoint(8, 10, begin, end);
+      createPoint(8, 11, begin+1, end+1);
+      createPoint(8, 12, begin+1, end+1);
+      break;
+
+    case '2':
+      createPoint(7, 6, begin+1, end+1);
+      createPoint(8, 6, begin+1, end+1);
+  
+      createPoint(4, 7, begin+1, end+1);
+      createPoint(12, 7, begin+1, end+1);
+  
+      createPoint(13, 8, begin, end);
+      createPoint(12, 9, begin, end);
+      createPoint(9, 10, begin, end);
+      createPoint(7, 11, begin, end);
+  
+      createPoint(6, 12, begin+1, end+1);
+      createPoint(8, 12, begin+1, end+1);
+      createPoint(10, 12, begin+1, end+1);
+      break;
+
+    case '3':
+
+      createPoint(7, 6, begin+1, end+1);
+      createPoint(8, 6, begin+1, end+1);
+  
+      createPoint(4, 7, begin, end);
+      createPoint(12, 7, begin, end);
+
+      createPoint(14, 8, begin, end);
+      createPoint(14, 9, begin, end);
+
+      createPoint(4, 10, begin, end);
+      createPoint(12, 10, begin, end);
+
+      createPoint(7, 11, begin+1, end+1);
+      createPoint(8, 11, begin+1, end+1);
+      break;
+
+    case '4':
+      
+      createPoint(10, 6, begin+1, end+1);
+      createPoint(5, 6, begin+1, end+1);
+      createPoint(10, 7, begin, end);
+      createPoint(5, 7, begin, end);
+      createPoint(6, 8, begin, end);
+      createPoint(10, 8, begin, end);
+      createPoint(10, 9, begin, end);
+      createPoint(10, 10, begin+1, end+1);
+      createPoint(10, 11, begin+1, end+1);
+      break;
+
+    case '5':
+      
+      createPoint(6, 6, begin+1, end+1);
+      createPoint(8, 6, begin+1, end+1);
+      createPoint(10, 6, begin+1, end+1);
+
+      createPoint(6, 7, begin, end);
+      createPoint(6, 8, begin, end);
+      createPoint(10, 9, begin, end);
+      createPoint(13, 10, begin, end);
+
+      createPoint(6, 11, begin+1, end+1);
+      createPoint(12, 11, begin+1, end+1);
+
+      createPoint(7, 12, begin+1, end+1);
+      createPoint(8, 12, begin+1, end+1);
+      break;
+
+    case '6':
+      createPoint(7, 5, begin+1, end+1);
+      createPoint(9, 5, begin+1, end+1);
+  
+      createPoint(4, 6, begin, end);
+      createPoint(4, 7, begin, end);
+  
+      createPoint(5, 8, begin, end);
+      createPoint(7, 8, begin, end);
+  
+      createPoint(4, 9, begin, end);
+      createPoint(12, 9, begin, end);
+  
+      createPoint(7, 10, begin+1, end+1);
+      createPoint(9, 10, begin+1, end+1);
+      break;
+
+    case '7':
+
+      createPoint(6, 6, begin+1, end+1);
+      createPoint(8, 6, begin+1, end+1);
+      createPoint(10, 6, begin+1, end+1);
+      
+      createPoint(3, 7, begin+1, end+1);
+      createPoint(14, 7, begin+1, end+1);
+    
+      createPoint(16, 8, begin, end);
+      createPoint(13, 9, begin, end);
+      createPoint(9, 10, begin, end);
+      createPoint(8, 11, begin, end);
+      createPoint(6, 12, begin+1, end+1);
+      createPoint(5, 13, begin+1, end+1);
+
+      break;
+
+    case '8':
+      createPoint(7, 6, begin+1, end+1);
+      createPoint(9, 6, begin+1, end+1);
+  
+      createPoint(4, 7, begin, end);
+      createPoint(12, 7, begin, end);
+  
+      createPoint(7, 8, begin, end);
+      createPoint(9, 8, begin, end);
+  
+      createPoint(4, 9, begin, end);
+      createPoint(12, 9, begin, end);
+  
+      createPoint(7, 10, begin+1, end+1);
+      createPoint(9, 10, begin+1, end+1);
+      break;
+
+    case '9':
+  
+  
+      createPoint(7, 6, begin+1, end+1);
+      createPoint(9, 6, begin+1, end+1);
+
+      createPoint(4, 7, begin, end);
+      createPoint(12, 7, begin, end);
+  
+      createPoint(10, 8, begin, end);
+      createPoint(8, 8, begin, end);
+  
+
+      createPoint(12, 9, begin, end);
+      createPoint(12, 10, begin, end);
+
+      createPoint(7, 11, begin+1, end+1);
+      createPoint(9, 11, begin+1, end+1);
+      break;
+
+    case '0':
+
+      createPoint(7, 6, begin+1, end+1);
+      createPoint(9, 6, begin+1, end+1);
+
+      createPoint(4, 7, begin, end);
+      createPoint(12, 7, begin, end);
+
+      createPoint(4, 8, begin, end);
+      createPoint(12, 8, begin, end);
+  
+      createPoint(4, 9, begin, end);
+      createPoint(12, 9, begin, end);
+  
+      createPoint(7, 10, begin+1, end+1);
+      createPoint(9, 10, begin+1, end+1);
+
+      break;
+
+  }
+}
+
+
+function addMessage(message) {
+  cursor = 20;
+  
+  for (let i=0; i < message.length; i++) {
+
+    console.log(cursor);
+
+    addChar(message.charAt(i), cursor, cursor+32);
+
+    cursor += 32;
+  }
+
+  FRAMES = (message.length + 1) * 32;
+
+  console.log(FRAMES);
+}
+
+
 function getRandomInt(min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-  }
-
-// // 1
-// createPoint(8, 6, 20, 52);
-// createPoint(8, 7, 20, 52);
-// createPoint(8, 8, 19, 52);
-// createPoint(8, 9, 19, 52);
-// createPoint(8, 10, 19, 52);
-// createPoint(8, 11, 20, 52);
-// createPoint(8, 12, 20, 52);
+}
 
 
-// // 2
-// createPoint(7, 6, 52, 84);
-// createPoint(8, 6, 52, 84);
-
-// createPoint(4, 7, 52, 84);
-// createPoint(12, 7, 52, 84);
-
-// createPoint(13, 8, 52, 84);
-// createPoint(12, 9, 52, 84);
-// createPoint(9, 10, 52, 84);
-// createPoint(7, 11, 52, 84);
-
-// createPoint(6, 12, 52, 84);
-// createPoint(8, 12, 52, 84);
-// createPoint(10, 12, 52, 84);
-
-
-// // 8
-// createPoint(7, 6, 84, 118);
-// createPoint(9, 6, 84, 118);
-
-// createPoint(4, 7, 84, 117);
-// createPoint(12, 7, 84, 117);
-
-// createPoint(7, 8, 84, 116);
-// createPoint(9, 8, 84, 116);
-
-// createPoint(4, 9, 84, 117);
-// createPoint(12, 9, 84, 117);
-
-// createPoint(7, 10, 84, 118);
-// createPoint(9, 10, 84, 118);
-
-const FADE = 30;
-const FRAMES = 256;
-const SPEED = 1.7;
+var FADE = 7;
+var FRAMES = 256;
+const SPEED = 1;
 
 const POINT_WIDTH = 0.29;
 const POINT_HEIGHT = 45;
@@ -160,6 +299,31 @@ window.addEventListener('resize', () => {
   resizeGraphics();
 });
 
+
+var pattern = '0123456789';
+
+var use_random_pattern = false;
+if (pattern == '') {
+  use_random_pattern = true;
+
+  for (let i=0; i<getRandomInt(1, 4); i++) {
+    createPoint(getRandomInt(1, PIXELS+1), getRandomInt(1, PIXELS+1), 50, 60);
+  }
+  for (let i=0; i<getRandomInt(1, 4); i++) {
+    createPoint(getRandomInt(1, PIXELS+1), getRandomInt(1, PIXELS+1), 120, 130);
+  }
+  for (let i=0; i<getRandomInt(1, 4); i++) {
+    createPoint(getRandomInt(1, PIXELS+1), getRandomInt(1, PIXELS+1), 190, 200);
+  }
+
+  FADE = 30;
+
+} else {
+
+  addMessage(pattern);
+
+}
+
 function setup() {
 
   const canvas = document.getElementById('sliding-grids-canvas');
@@ -176,7 +340,7 @@ function setup() {
   
   resizeGraphics();
   
-  frameRate(30);
+  frameRate(60);
 
   console.log('p5 script ran');
 
@@ -201,7 +365,7 @@ function draw() {
   
   var frame = Math.floor(frameCount * SPEED) % FRAMES;
   
-  if (frame == 0) {
+  if (frame == 0 && use_random_pattern) {
     points = [];
     
     for (let i=0; i<getRandomInt(1, 4); i++) {
